5797f3f1e9eaa3360d40c501b7cce1ce9a57b85e
Yang Xu <yang.xu3110@gmail.com>
2016-12-08 15:08:53 +0900

    Features done

--- src/main/java/Model/Feature.java
+++ src/main/java/Model/Feature.java
@@ -39,71 +39,68 @@ public class Feature {
             return prevFeatures.get(prevFeatures.size() - 1).getCurUnit();
         }else return null;
     }
-    public void updateWithCommand(List<Double> values){
-        double a1b3 = getDistance(getPrevUnit().getPosition(),command.getTargetPos());
-        values.add(a1b3);
-        double a2b3 = getDistance(getPrevUnit().getTargetPosition(),command.getTargetPos()) ;
-        values.add(a2b3);
-        double a3b3 = getDistance(prevFeatures.get(prevFeatures.size()-1).getCommand().getTargetPos(),command.getTargetPos());
-        values.add(a3b3);
+    private Command getPrevCommand(){
+        if(prevFeatures.size()>0){
+            return prevFeatures.get(prevFeatures.size()-1).getCommand();
+        }
+        else return null;
     }
 
     public void initFeature(){
-        double isEnemy;
-        double unitType;
-        double HP;
-        double shield;
-        double CD;
-        double curCommandType;
-        double nextCommandType;
-        double curUnitType;
-        //a1:u.pos,a2: u.cur_cmd.target_pos,a3:u.next_cmd.target_pos
-        //b1:uk+1.pos, b2:uk+1.cur_cmd.target_pos, b3:c.target_pos
-        double a1b1;
-        double a1b2;
-        double a2b1;
-        double a2b2;
-        double a3b1;
-        double a3b2;
-
-
-        isEnemy = subState.getGame().self().equals(curUnit.getPlayer())?1:0;
-        vals.add(isEnemy);
-        // type always marine ATM
-        unitType = curUnit.getType().equals(UnitType.Terran_Marine)?1:0;
-        vals.add(unitType);
-        HP = curUnit.getHitPoints();
+        double HP= curUnit.getHitPoints();;
+        double shield = curUnit.getShields();
+        double CD = curUnit.getGroundWeaponCooldown();
+        double isEnemy = subState.getGame().self().equals(curUnit.getPlayer())?1:0;
+        double unitType = curUnit.getType().equals(UnitType.Terran_Marine)?1:0;
         vals.add(HP);
-        shield = curUnit.getShields();
         vals.add(shield);
-        CD = curUnit.getGroundWeaponCooldown();
         vals.add(CD);
-        //!!!!!!!!!!!!!!
-        UnitCommand prevCommand = curUnit.getLastCommand();
-        curCommandType =Command.parseCommandType(prevCommand.getUnitCommandType()).getVal();
-        vals.add(curCommandType);
-        //!!!!!!!!!!!!!
-        nextCommandType = command.getCommandType().getVal();
-        vals.add(nextCommandType);
+        vals.add(isEnemy);
+        vals.add(unitType);
 
-        curUnitType = curUnit.getType().equals(UnitType.Terran_Marine)?1:0;
-        vals.add(curUnitType);
-        a1b1 = getDistance(getPrevUnit().getPosition(),curUnit.getPosition());
-        vals.add(a1b1);
-        a1b2 = getDistance(getPrevUnit().getPosition(),curUnit.getTargetPosition());
-        vals.add(a1b2);
-        a2b1 = getDistance(getPrevUnit().getTargetPosition(),curUnit.getPosition());
-        vals.add(a2b1);
-        a2b2 = getDistance(getPrevUnit().getTargetPosition(),curUnit.getTargetPosition());
-        vals.add(a2b2);
-        a3b1 = getDistance(prevFeatures.get(prevFeatures.size()-1).getCommand().getTargetPos(),curUnit.getPosition());
-        vals.add(a3b1);
-        a3b2 = getDistance(prevFeatures.get(prevFeatures.size()-1).getCommand().getTargetPos(),curUnit.getLastCommand().getTargetPosition());
-        vals.add(a3b2);
+        initRelativeFeatures();
+
+        Command.cType prevCmdType =Command.parseCommandType
+                (curUnit.getLastCommand().getUnitCommandType());
+        vals.add((double)prevCmdType.getVal());
 
     }
 
+    private void initRelativeFeatures() {
+        Position curUnitPosition = curUnit.getPosition();
+        Position curUnitTargetPosition = curUnit.getTargetPosition();
+        Position nextCommandTargetPosition = command.getTargetPos();
+
+        Position prevUnitPosition = getPrevUnit()==null?null:getPrevUnit().getPosition();
+        Position prevUnitTargetPosition = getPrevUnit()==null?null:getPrevUnit().getPosition();
+        Position prevUnitCommandTargetPosition = getPrevCommand()==null?null:getPrevCommand().getTargetPos();
+
+        double a1b1 = getDistance(curUnitPosition,prevUnitPosition);
+        double a1b2 = getDistance(curUnitPosition,prevUnitTargetPosition);
+        double a1b3 = getDistance(curUnitPosition,prevUnitCommandTargetPosition);
+
+        vals.add(a1b1);
+        vals.add(a1b2);
+        vals.add(a1b3);
+
+        double a2b1 = getDistance(curUnitTargetPosition,prevUnitPosition);
+        double a2b2 = getDistance(curUnitTargetPosition,prevUnitTargetPosition);
+        double a2b3 = getDistance(curUnitTargetPosition,prevUnitCommandTargetPosition);
+
+        vals.add(a2b1);
+        vals.add(a2b2);
+        vals.add(a2b3);
+
+        double a3b1 = getDistance(nextCommandTargetPosition,prevUnitPosition);
+        double a3b2 = getDistance(nextCommandTargetPosition,prevUnitTargetPosition);
+        double a3b3 = getDistance(nextCommandTargetPosition,prevUnitCommandTargetPosition);
+        vals.add(a3b1);
+        vals.add(a3b2);
+        vals.add(a3b3);
+    }
+
     private double getDistance(Position pos1, Position pos2){
+        if(pos1 == null||pos2 == null) return 0;
         double a = Math.pow(pos1.getX()-pos2.getX(),2);
         double b = Math.pow(pos1.getY()-pos2.getY(),2);
         return Math.sqrt(a+b);
